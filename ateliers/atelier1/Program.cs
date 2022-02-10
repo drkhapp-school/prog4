f187435dde3b01505407b0d6b26d6b0a526bb4bc
darkhappy <darkhappyy@gmail.com>
2022-02-10 12:59:42 -0500

    Intro to Csharp

---
+++ ateliers/atelier1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace atelier1
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      int firstNumber;
+      int secondNumber;
+      int result;
+      
+      Console.WriteLine("First number:");
+      firstNumber = Convert.ToInt32(Console.ReadLine());
+      
+      Console.WriteLine("Second number:");
+      secondNumber = Convert.ToInt32(Console.ReadLine());
+
+      result = firstNumber + secondNumber;
+      Console.WriteLine("Answer: " + result);
+    }
+  }
+}
